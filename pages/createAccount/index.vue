b1179a8a94944b46f13e52980d3949712ff70500
広瀬雅季 <hiromasaok.com@gmail.com>
2019-12-11 15:11:26 +0900

    nuxtjsの設定変更

---
+++ pages/createAccount/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        nuxt
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class index extends Vue {
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,8 @@
   <div class="container">
     <div>
       <h1 class="title">
-        nuxt_spa
+        ログイン
       </h1>
-      <h2 class="subtitle">
-        My tremendous Nuxt.js project
-      </h2>
       <Form />
       <div class="links">
         <a
@@ -14,15 +11,12 @@
           target="_blank"
           class="button--green"
         >
-          Documentation
+          ログイン
         </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <br />
+        <nuxt-link to="/createAccount">
+          アカウント作成
+        </nuxt-link>
       </div>
     </div>
   </div>
@@ -38,39 +32,8 @@ import Form from '@/components/Form.vue';
   },
 })
 export default class index extends Vue {
-  
+  $refs!: {
+    form: Form,
+  }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
